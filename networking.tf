4f00051ffef9a274c2755c2939a22d24e60dc20c
bia andersson <tobias.andersson@comeon.com>
2022-11-03 00:29:25 +0100

    refactoring of resources ~> added networking configuration file

---
+++ networking.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "mtc-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "mtc-internet-gateway"
+  }
+}
+
