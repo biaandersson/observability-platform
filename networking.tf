41bb6ce0bc64994584d1139a6e79f3147f5bee63
bia andersson <tobias.andersson@comeon.com>
2022-11-03 13:42:17 +0100

    added route_table and default routing

--- networking.tf
+++ networking.tf
@@ -1,10 +1,18 @@
+resource "random_id" "random" {
+  byte_length = 2
+}
+
 resource "aws_vpc" "mtc_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   tags = {
-    Name = "mtc-vpc"
+    Name = "mtc-vpc-${random_id.random.dec}"
   }
 }
 
@@ -12,7 +20,29 @@ resource "aws_internet_gateway" "mtc_internet_gateway" {
   vpc_id = aws_vpc.mtc_vpc.id
 
   tags = {
-    Name = "mtc-igw"
+    Name = "mtc-igw-${random_id.random.dec}"
+  }
+}
+
+resource "aws_route_table" "mtc_public_route_table" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "mtc-public"
+  }
+}
+
+resource "aws_route" "mtc_default_route" {
+  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
+  route_table_id         = aws_route_table.mtc_public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_default_route_table" "mtc_private_route_table" {
+  default_route_table_id = aws_vpc.mtc_vpc.default_route_table_id
+
+  tags = {
+    Name = "mtc-private"
   }
 }
 
